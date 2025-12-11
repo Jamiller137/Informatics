3a89c47dbbf564afcbf16d83399898f7ace686d2
Jacob Miller <Jamiller219@gmail.com>
2025-12-11 12:51:01 -0600

    hwk8: run fmt

--- homework/jacob_miller_hwk8.py
+++ homework/jacob_miller_hwk8.py
@@ -1,5 +1,5 @@
-'''
-Homework 8 
+"""
+Homework 8
 
 Total: 30 points (will be converted to 5 points)
 
@@ -16,13 +16,15 @@ https://networkx.org/documentation/stable/reference/index.html
 
 Networkx tutorial:
 https://networkx.org/documentation/stable/tutorial.html
-'''
+"""
+
 import networkx as nx
 
 G = nx.DiGraph()
 
+
 def read_data_into_graph():
-    '''
+    """
     (1 point)
 
     Parameters: None
@@ -31,12 +33,12 @@ def read_data_into_graph():
     populates the directed graph G with this data.
 
     Returns: None
-    '''
+    """
     global G
     # Assuming that the reddit file contains directed graph x -> y
     # With no errors in the file:
 
-    with open('reddit_file.txt', 'r') as file:
+    with open("reddit_file.txt", "r") as file:
         for line in file:
             parsed = line.split()
 
@@ -49,7 +51,7 @@ def read_data_into_graph():
 
 
 def get_num_of_nodes_edges():
-    '''
+    """
     (1 point)
 
     Parameters: None
@@ -58,7 +60,7 @@ def get_num_of_nodes_edges():
 
     Returns: a tuple where the first element is the number of nodes
     in 'G' and the second element is the number of edges in 'G'.
-    '''
+    """
 
     global G
 
@@ -66,50 +68,50 @@ def get_num_of_nodes_edges():
 
 
 def get_nodes_with_most_outgoing_edges(N: int):
-    '''
+    """
     (6 points)
 
     Parameter: N, number of top ranked nodes of interest
-   
+
     This function identifies the top N nodes in G, where ranking is by
     the number of outward directed edges.
-    
+
     Returns: a list of tuples, each tuple contains a node identifier
     (nodeid or label) and the count of its outward directed edges.
             The list is sorted by this count in descending order.
-    '''
+    """
     global G
 
-    results = [ (node, G.out_degree(node) ) for node in G ]
+    results = [(node, G.out_degree(node)) for node in G]
 
     return sorted(results, key=lambda x: x[1], reverse=True)[:N]
 
 
 def get_nodes_with_most_incoming_edges(N: int):
-    '''
+    """
     (6 points)
 
     Parameter: N, number of top ranked nodes of interest
 
     This function identifies the top N nodes in G, where ranking is by the
     number of inward directed edges.
-        
+
     Returns: a list of tuples, each tuple contains a node identifier
     (nodeid or label) and the count of its inward directed edges.
             The list is sorted by this count in descending order.
-    '''
+    """
 
     # SAME APPROACH AS OUTGOING EDGES:
-        # just now using in_degree
+    # just now using in_degree
     global G
-    
-    results = [ (node, G.in_degree(node) ) for node in G ]
+
+    results = [(node, G.in_degree(node)) for node in G]
 
     return sorted(results, key=lambda x: x[1], reverse=True)[:N]
 
 
 def get_num_of_connected_components():
-    '''
+    """
     (3 points)
 
     Parameters: None
@@ -119,32 +121,35 @@ def get_num_of_connected_components():
     Returns: a tuple of two integers. The first is the number of strongly
             connected components and the second is the number of weakly
             connected components.
-    '''
+    """
     global G
 
-    return ( nx.number_strongly_connected_components(G), nx.number_weakly_connected_components(G) )
+    return (
+        nx.number_strongly_connected_components(G),
+        nx.number_weakly_connected_components(G),
+    )
 
 
 def get_nth_largest_strongly_connected_component(N: int):
-    '''
+    """
     (5 points)
 
     Parameters: N, this represents a specific rank.
     1 means the largest connected component in number of nodes,
     2 means the second largest and so on.
-    
+
     The function identifies the Nth largest component out of all the
     strongly connected components in G.
     It then returns the number of nodes, number of edges and the average shortest
     path length within this component.
-    
+
     Returns: a list containing information in the following order for the
-                selected component: number of nodes, number of edges, 
-                the average shortest path length. 
+                selected component: number of nodes, number of edges,
+                the average shortest path length.
                 If there does not exist an Nth largest member of the strongly
                 connected components then return [None, 0, 0]
 
-    '''
+    """
     global G
 
     # get list of sorted connected components (uses documentation example)
@@ -154,7 +159,7 @@ def get_nth_largest_strongly_connected_component(N: int):
     if len(comp_list) < N:
         return [None, 0, 0]
 
-    component = G.subgraph(comp_list[N-1])
+    component = G.subgraph(comp_list[N - 1])
 
     num_nodes = nx.number_of_nodes(component)
     num_edges = nx.number_of_edges(component)
@@ -163,23 +168,21 @@ def get_nth_largest_strongly_connected_component(N: int):
     return [num_nodes, num_edges, avg_shortest_path]
 
 
-
-
 def distance(D: int, N):
-    '''
+    """
     (5 points)
 
-    Parameter: 
+    Parameter:
         N: a nodeid (aka node label),
         D: a distance. To clarify, in x --> y --> z, x and z are distance 2 apart
         while x and y are distance 1 apart.
-    
+
         This function returns the set of nodes in G that are at distance D to
         a given node N.
-    
+
     Returns: a set of nodes (nodieid/label) that are at distance D from node N.
             If no such nodes exist, returns an empty set.
-    '''
+    """
 
     global G
     # is a set, however if N is not a node it can throw an error:
@@ -191,22 +194,20 @@ def distance(D: int, N):
 
 
 def weakly_connected_components(N: int):
-    '''           
+    """
     (2 points)
 
     Parameters: N, component size (i.e., N is the number of nodes).
 
     This function returns a list containing SUBGRAPHS, one for each weakly
     connected component in G of size N.
-    
+
     Returns: a list of subgraphs as described above
-    '''
+    """
 
     global G
 
     # I am assuming subgraph means the subgraph type in networkx
-        # and not the list of nodes obtained from 
-        # nx.weakly_connected_components
-    return [G.subgraph(x) for x in nx.weakly_connected_components(G) if len(x) == N ]
-
-    
+    # and not the list of nodes obtained from
+    # nx.weakly_connected_components
+    return [G.subgraph(x) for x in nx.weakly_connected_components(G) if len(x) == N]
